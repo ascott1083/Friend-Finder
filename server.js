81c1b9e0372517b075549470cf15d9a62f3983a4
AJ <ascott1083@yahoo.com>
2019-10-27 21:00:17 -0500

    added server.js code

---
+++ server.js
@@ -0,0 +1,15 @@
+let path = require("path")
+let express = require("express")
+
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
