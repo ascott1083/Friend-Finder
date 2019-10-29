aa3da0907f10fcf5300c1141bf4027ba226b49ae
AJ <ascott1083@yahoo.com>
2019-10-28 20:44:33 -0500

    added friends code

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+const friends = [
+    {
+      name: "Chandler Bing",
+      photo:
+        "https://www.stylist.co.uk/images/app/uploads/2017/10/24195115/34fb7869e89b21c70d0662c6af04fb53-828x552.jpg?w=1200&h=1&fit=max&auto=format%2Ccompress",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Rachel Green",
+      photo:
+        "https://1.bp.blogspot.com/-AyX4vgtlNuM/VcKHZGs7nLI/AAAAAAAABOE/SrXI6GntXjY/s1600/Jennifer-Aniston-Rachel-Green-rachel-green-17161614-432-325.jpg",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+      name: "Ross Geller",
+      photo:
+        "https://purewows3.imgix.net/images/articles/2019_02/ross-geller-friends.jpg?auto=format,compress&cs=strip&fit=min&w=600&h=315",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Phoebe Buffay",
+      photo:
+        "https://play-images-prod-cms.tech.tvnz.co.nz/api/v1/web/image/content/dam/images/entertainment/shows/f/friends/articles/friends-characters-phoebe.jpg.2017-11-27T17:11:49+13:00.jpg?width=960&height=540",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+      name: "Joey Tribbiani",
+      photo:
+        "http://cdn3.whatculture.com/images/2019/05/18c8a6d571330b00-600x338.jpg",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+      name: "Monica Geller",
+      photo:
+        "https://66.media.tumblr.com/6d2ff8fd01b0fed6b149cffc7087a7a8/tumblr_orreag04Q01ubcxtyo6_250.png",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+  ];
+  module.exports = friends;
