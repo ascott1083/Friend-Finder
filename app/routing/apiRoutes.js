8d5cfdfadc3c94ca8a5d63825969ff8be6cda59d
AJ <ascott1083@yahoo.com>
2019-10-28 20:20:17 -0500

    added api route

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,40 @@
+const path = require("path");
+
+module.exports = function (app){
+    let friendsList = [];
+    let bestMatches = [];
+    
+   
+    app.get("/api/friends", function(req, res) {
+    res.json(friendsList);
+    });
+
+    app.post("/api/friends",  function(req, res){
+    let currentFriend = req.body;
+        
+        let friendDifferences = [];
+
+        if (friendsList.length >= 1){
+            bestMatch = {};
+            bestDiff = 100;
+            for (let i in friendsList) {
+                totalDiff = 0;
+                
+                for (let j in currentFriend.scores) {
+                    totalDiff += Math.abs(currentFriend.scores[j]-friendsList[i].scores[j]);
+                }
+                if (bestDiff > totalDiff) {
+                    bestDiff = totalDiff;
+                    bestMatch = friendsList[i];
+                }
+            }
+            bestMatches.push([currentFriend, bestMatch]);
+            res.json(bestMatch);
+        } else {        
+            res.json(currentFriend);
+        }
+
+        friendsList.push(currentFriend);
+        
+    });
+};
