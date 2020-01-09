46180ce56344bc772dd17f083e5f19eb32b63c1b
AJ <ascott1083@yahoo.com>
2020-01-09 12:28:08 -0600

    corrected scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,103 @@
-const friends = [
-    {
-      name: "Chandler Bing",
-      photo:
-        "https://www.stylist.co.uk/images/app/uploads/2017/10/24195115/34fb7869e89b21c70d0662c6af04fb53-828x552.jpg?w=1200&h=1&fit=max&auto=format%2Ccompress",
-      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
-    },
-    {
-      name: "Rachel Green",
-      photo:
-        "https://1.bp.blogspot.com/-AyX4vgtlNuM/VcKHZGs7nLI/AAAAAAAABOE/SrXI6GntXjY/s1600/Jennifer-Aniston-Rachel-Green-rachel-green-17161614-432-325.jpg",
-      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
-    },
-    {
-      name: "Ross Geller",
-      photo:
-        "https://purewows3.imgix.net/images/articles/2019_02/ross-geller-friends.jpg?auto=format,compress&cs=strip&fit=min&w=600&h=315",
-      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
-    },
-    {
-      name: "Phoebe Buffay",
-      photo:
-        "https://play-images-prod-cms.tech.tvnz.co.nz/api/v1/web/image/content/dam/images/entertainment/shows/f/friends/articles/friends-characters-phoebe.jpg.2017-11-27T17:11:49+13:00.jpg?width=960&height=540",
-      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
-    },
-    {
-      name: "Joey Tribbiani",
-      photo:
-        "http://cdn3.whatculture.com/images/2019/05/18c8a6d571330b00-600x338.jpg",
-      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
-    },
-    {
-      name: "Monica Geller",
-      photo:
-        "https://66.media.tumblr.com/6d2ff8fd01b0fed6b149cffc7087a7a8/tumblr_orreag04Q01ubcxtyo6_250.png",
-      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
-    },
-  ];
-  module.exports = friends;
+
+var friends = [
+  {
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Jacob Deming",
+    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Jeremiah Scanlon",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Louis T. Delia",
+    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Lou Ritter",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Jordan Biason",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
